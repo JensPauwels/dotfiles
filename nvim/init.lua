e15329b5e62018f13f4900a24fcf9e18c1064d29
Jens Pauwels <jenspauwels@Jenss-MBP.localdomain>
2023-03-01 19:58:01 +0100

    Updated lazy config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,12 @@
-require("config.lazy")
-
+require('lazy-c')
 require('opts')
 require('maps')
 require('style')
+require('coq-c')
+require('lsp-c')
+require('monokai-c')
+require('supertab-c')
+require('treesitter-c')
 
 -- require('coqConfig')
 -- require('tokyonightConfig')
--- nvim/lua/lazy-c.lua
+++ nvim/lua/lazy-c.lua
@@ -1,4 +1,18 @@
-return {
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+ if not vim.loop.fs_stat(lazypath) then
+   vim.fn.system({
+     "git",
+     "clone",
+     "--filter=blob:none",
+     "--single-branch",
+     "https://github.com/folke/lazy.nvim.git",
+     lazypath,
+   })
+ end
+
+vim.opt.runtimepath:prepend(lazypath)
+
+require("lazy").setup({
   -- Spell checking
   'kamykn/spelunker.vim',
   -- Themes
@@ -77,4 +91,4 @@ return {
     {'nvim-lua/plenary.nvim'}
   }
 }
-}
+}, {})
