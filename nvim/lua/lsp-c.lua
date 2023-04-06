766b334f6f685f203eaf2fc5e22ae8660fad19e9
Jens Pauwels <jenspauwels@Jenss-MacBook-Pro.local>
2023-04-06 08:08:09 +0200

    Added eslint lsp to my config

--- nvim/lua/lsp-c.lua
+++ nvim/lua/lsp-c.lua
@@ -27,14 +27,10 @@ local on_attach = function(client, bufnr)
 
 end
 
--- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
--- local capabilities = vim.lsp.protocol.make_client_capabilities()
--- capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
--- capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'tsserver', 'cssls', 'gopls', 'svelte' }
+local servers = { 'tsserver', 'cssls', 'gopls', 'svelte', 'eslint' }
 
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup(
--- nvim/lua/style.lua
+++ nvim/lua/style.lua
@@ -2,6 +2,7 @@ vim.cmd('colorscheme monokai-pro')
 vim.cmd(':highlight LineNr guifg=#808691')
 vim.cmd('highlight  DiagnosticUnderlineHint ctermfg=243 guifg=#65737e')
 vim.cmd('highlight  DiagnosticUnderlineError guifg=#CB5551 guibg=#342C3C')
+vim.api.nvim_set_hl(0, "ColorColumn", { ctermbg = 0, bg = "#161821" })
 -- vim.g:lightline = {'colorscheme': 'monokaipro'}
 
 -- vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
