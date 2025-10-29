8ec6b39ed5112ce8166fcf7fc69a5c31cb39d5ea
JensPauwels <jens.pauwels@hotmail.com>
2025-10-29 08:02:28 +0100

    Setup nvchad

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,21 +1,20 @@
-require "nvchad.mappings"
+require("nvchad.mappings")
 
 -- add yours here
 
-local map = vim.keymap.set
-
-map("i", "jk", "<ESC>")
-map("n", "<C-p>", "<cmd>Telescope find_files<cr>")
-map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Telescope live grep" })
-map("n", "<leader>n", "<cmd>NvimTreeToggle<cr>", { desc = "Toggle nvim-tree" })
-map("n", "<leader>s", "<cmd>NvimTreeFindFile<cr>", { desc = "Reveal file in nvim-tree" })
-map("n", "<leader>gr", "<cmd>Telescope lsp_references<cr>", { desc = "LSP references" })
-map('n', '<leader>e', vim.diagnostic.open_float, { desc = '[E]xpand diagnostic message' })
-map('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
-map("n", "<C-t>", function ()
+vim.keymap.set("i", "jk", "<ESC>")
+vim.keymap.set("n", "<C-p>", "<cmd>Telescope find_files<cr>")
+vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>n", "<cmd>NvimTreeToggle<cr>", { desc = "Toggle nvim-tree" })
+vim.keymap.set("n", "<leader>s", "<cmd>NvimTreeFindFile<cr>", { desc = "Reveal file in nvim-tree" })
+vim.keymap.set("n", "<leader>gr", "<cmd>Telescope lsp_references<cr>", { desc = "LSP references" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "[E]xpand diagnostic message" })
+vim.keymap.set("n", "<leader>ca", "<cmd>LspTypescriptSourceAction<cr>", { desc = "Code actions" })
+vim.keymap.set("n", "<leader>da", "<cmd>Telescope diagnostics<cr>", { desc = "Telescope diagnostics" })
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("n", "<C-t>", function()
   require("nvchad.themes").open()
 end)
 
 -- Lazygit
-map("n", "<leader>gg", "<cmd>LazyGit<cr>", { desc = "Open LazyGit" })
+vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<cr>", { desc = "Open LazyGit" })
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,15 +1,62 @@
 return {
   {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        opts = {
+          ensure_installed = {
+            "delve",
+            "js-debug-adapter",
+          },
+        },
+      },
+      "rcarriga/nvim-dap-ui",
+    },
+    config = function()
+      print("Loading nvim-dap configuration...")
+      local dap = require("dap")
+      local dapui = require("dapui")
+
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+
+      vim.keymap.set("n", "<F5>", dap.continue)
+      vim.keymap.set("n", "<F10>", dap.step_over)
+      vim.keymap.set("n", "<F11>", dap.step_into)
+      vim.keymap.set("n", "<F12>", dap.step_out)
+      vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint)
+      vim.keymap.set("n", "<leader>dr", dap.repl.open)
+
+      dapui.setup()
+    end,
+  },
+  {
+    "vim-test/vim-test",
+    config = function()
+      vim.keymap.set("n", "<leader>tn", ":TestNearest<CR>")
+      vim.keymap.set("n", "<leader>tf", ":TestFile<CR>")
+    end,
+  },
+
+  {
     "stevearc/conform.nvim",
     event = "BufWritePre", -- uncomment for format on save
-    opts = require "configs.conform",
+    opts = require("configs.conform"),
   },
 
   -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
-      require "configs.lspconfig"
+      require("configs.lspconfig")
     end,
   },
 
@@ -19,7 +66,7 @@ return {
     version = "*",
     event = "VeryLazy",
     config = function()
-      require("nvim-surround").setup {}
+      require("nvim-surround").setup({})
     end,
   },
 
@@ -44,98 +91,4 @@ return {
     cmd = "Spectre",
     config = true,
   },
-
-  {
-    "mfussenegger/nvim-dap",
-    event = "VeryLazy", -- load on startup or any buffer
-    config = function()
-      -- Basic keymaps for nvim-dap
-      local dap = require "dap"
-      vim.keymap.set("n", "<F5>", dap.continue)
-      vim.keymap.set("n", "<F10>", dap.step_over)
-      vim.keymap.set("n", "<F11>", dap.step_into)
-      vim.keymap.set("n", "<F12>", dap.step_out)
-      vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint)
-      vim.keymap.set("n", "<leader>dr", dap.repl.open)
-      -- Go adapter setup
-      dap.adapters.go = {
-        type = "executable",
-        command = "node",
-        args = { os.getenv "HOME" .. "/go/bin/go-debug-adapter" },
-      }
-      dap.configurations.go = {
-        {
-          type = "go",
-          name = "Debug",
-          request = "launch",
-          program = "${file}",
-        },
-      }
-      -- Node.js adapter setup
-      dap.adapters.node2 = {
-        type = "executable",
-        command = "node",
-        args = {
-          os.getenv "HOME"
-            .. "/.local/share/nvim/dap_adapters/vscode-node-debug2/out/src/nodeDebug.js",
-        },
-      }
-      dap.configurations.javascript = {
-        {
-          type = "node2",
-          request = "launch",
-          name = "Launch Program",
-          program = "${file}",
-          cwd = vim.fn.getcwd(),
-          sourceMaps = true,
-          protocol = "inspector",
-        },
-      }
-      dap.configurations.typescript = dap.configurations.javascript
-    end,
-  },
-
-  {
-    "nvim-neotest/neotest",
-    event = "VeryLazy",
-    dependencies = {
-      "nvim-neotest/nvim-nio",
-      "nvim-lua/plenary.nvim",
-      "antoinemadec/FixCursorHold.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-neotest/neotest-go",
-    },
-    config = function()
-      -- get neotest namespace (api call creates or returns namespace)
-      local neotest_ns = vim.api.nvim_create_namespace "neotest"
-      vim.diagnostic.config({
-        virtual_text = {
-          format = function(diagnostic)
-            local message =
-              diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
-            return message
-          end,
-        },
-      }, neotest_ns)
-      require("neotest").setup {
-        -- your neotest config here
-        adapters = {
-          require "neotest-go",
-        },
-      }
-    end,
-  },
-
-  -- test new blink
-  -- { import = "nvchad.blink.lazyspec" },
-
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
 }
